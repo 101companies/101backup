2e3dd6d63155ba71c22cfc599b3dd42646d728d6
101companies <softlang@uni-koblenz.de>
2014-06-03 17:23:51 +0200

    New content

---
+++ 101wiki/Technology:java.io
@@ -0,0 +1,10 @@
+== Headline == 
+
+Java's I/O framework
+
+== Metadata ==
+
+* [[instanceOf::Framework]]
+* [[instanceOf::Java package]]
+* [[identifies::http://docs.oracle.com/javase/7/docs/api/java/io/package-summary.html]]
+* [[instanceOf::Namespace:Technology]]
---
+++ 101wiki/Technology:java.sql
@@ -0,0 +1,10 @@
+== Headline == 
+
+The Java API for accessing and processing data stored in a data source
+
+== Metadata ==
+
+* [[instanceOf::Library]]
+* [[instanceOf::Java package]]
+* [[identifies::http://docs.oracle.com/javase/7/docs/api/java/sql/package-summary.html]]
+* [[instanceOf::Namespace:Technology]]
